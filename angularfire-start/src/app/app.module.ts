87e22cc24b7d6b5002e540f50d730ecdf96c233c
Lucas Dixon (iislucas) <ldixon@google.com>
2024-01-17 01:07:30 +0100

    initial fixes for codelab

--- angularfire-start/src/app/app.module.ts
+++ angularfire-start/src/app/app.module.ts
@@ -36,8 +36,46 @@ import { HeaderComponent } from './components/header/header.component';
     ChatPageComponent,
     HeaderComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, CommonModule, FormsModule],
-  providers: [],
+  imports: [
+    BrowserModule, AppRoutingModule, CommonModule, FormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => {
+        const auth = getAuth();
+        if (location.hostname === 'localhost') {
+            connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
+        }
+        return auth;
+    }),
+    provideFirestore(() => {
+        const firestore = getFirestore();
+        if (location.hostname === 'localhost') {
+            connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
+        }
+        return firestore;
+    }),
+    provideFunctions(() => {
+        const functions = getFunctions();
+        if (location.hostname === 'localhost') {
+            connectFunctionsEmulator(functions, '127.0.0.1', 5001);
+        }
+        return functions;
+    }),
+    provideStorage(() => {
+        const storage = getStorage();
+        if (location.hostname === 'localhost') {
+            connectStorageEmulator(storage, '127.0.0.1', 5001);
+        }
+        return storage;
+    }),
+    provideMessaging(() => {
+        return getMessaging();
+    }),
+  ],
+  providers: [
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+
+
+}
